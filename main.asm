b8598363e015e5907da0104099ee4a4c2df5595c
Lyblinskii Alexander <lyblinskii.shx@gmail.com>
2014-12-19 20:15:51 -0600

    Added proc to view generalpurpose regs values as AX, BX, CX, DX

--- main.asm
+++ main.asm
@@ -13,12 +13,17 @@
 mov bx, boot_string			; Load address of greeting
 call print_sz				; Print @boot_string on screen
 
-mov dx, 0x0AF1
-call print_hex
+mov ax, 1
+mov bx, 2
+mov cx, 0x35
+mov dx, 10
+call print_cpu_st			; Print @AX, @BX, @DX, @CX values
 jmp endless_loop
 
+%include "print_nl.asm"
 %include "print_sz.asm"		; include print_sz procedure
 %include "print_hex.asm"		; include print_sz procedure
+%include "print_cpu_st.asm"
 
 endless_loop:
 	jmp $					; endless loop
